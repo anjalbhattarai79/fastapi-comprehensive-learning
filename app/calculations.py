b0a2082d32b8e1df66caba3344339efaccbfa940
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2026-03-12 13:51:19 +0545

    Pytest on class level; implementation of fixtures

--- app/calculations.py
+++ app/calculations.py
@@ -1,11 +1,36 @@
-def add (a:int, b:int) -> int:
-    return a + b    
+def add(num1: int, num2: 2):
+    return num1 + num2
 
-def substract(a:int, b:int) -> int:
-    return a - b    
 
-def multiply(a:int, b:int) -> int:
-    return a * b
+def subtract(num1: int, num2: int):
+    return num1 - num2
 
-def divide(a:int, b:int) -> float:
-    return a / b
+
+def multiply(num1: int, num2: int):
+    return num1 * num2
+
+
+def divide(num1: int, num2: int):
+
+    return num1 / num2
+
+
+class InsufficientFunds(Exception):
+    pass
+
+
+class BankAccount():
+    def __init__(self, starting_balance=0):
+        self.balance = starting_balance
+
+    def deposit(self, amount):
+        self.balance += amount
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            raise InsufficientFunds("Insufficient funds in account")
+
+        self.balance -= amount
+
+    def collect_interest(self):
+        self.balance *= 1.1
--- tests/test_calculations.py
+++ tests/test_calculations.py
@@ -1,28 +1,81 @@
 import pytest
-from app.calculations import add, multiply, substract, divide
+from app.calculations import add, subtract, multiply, divide, BankAccount, InsufficientFunds
 
-@ pytest.mark.parametrize("a, b, expected", [
-    (2, 3, 5),
-    (1, 1, 2),
-    (0, 0, 0)
-])
-def test_add(a, b, expected):
-    print('Testing add function')
-    assert add(a, b) == expected
 
-@ pytest.mark.parametrize("a, b, expected", [
-    (5, 3, 2),
-    (10, 4, 6),
-    (0, 0, 0)
+@pytest.fixture
+def zero_bank_account():
+    print("creating empty bank account")
+    return BankAccount()
+
+
+@pytest.fixture
+def bank_account():
+    return BankAccount(50)
+
+
+@pytest.mark.parametrize("num1, num2, expected", [
+    (3, 2, 5),
+    (7, 1, 8),
+    (12, 4, 16)
 ])
-def test_substract(a, b, expected):
-    print('Testing substract function')
-    assert substract(a, b) == expected
+def test_add(num1, num2, expected):
+    print("testing add function")
+    assert add(num1, num2) == expected
+
+
+def test_subtract():
+    assert subtract(9, 4) == 5
+
 
 def test_multiply():
-    print('Testing multiply function')
-    assert multiply(2, 3) == 6
-    
-def test_divide():
-    print('Testing divide function')
-    assert divide(6, 3) == 2
+    assert multiply(4, 3) == 12
+
+
+def test_divde():
+    assert divide(20, 5) == 4
+
+
+def test_bank_set_initial_amount(bank_account):
+
+    assert bank_account.balance == 50
+
+
+def test_bank_default_amount(zero_bank_account):
+    print("testing my bank account")
+    assert zero_bank_account.balance == 0
+
+
+def test_withdraw(bank_account):
+
+    bank_account.withdraw(20)
+    assert bank_account.balance == 30
+
+
+def test_deposit(bank_account):
+
+    bank_account.deposit(30)
+    assert bank_account.balance == 80
+
+
+def test_collect_interest(bank_account):
+
+    bank_account.collect_interest()
+    assert round(bank_account.balance, 6) == 55
+
+
+@pytest.mark.parametrize("deposited, withdrew, expected", [
+    (200, 100, 100),
+    (50, 10, 40),
+    (1200, 200, 1000)
+
+])
+def test_bank_transaction(zero_bank_account, deposited, withdrew, expected):
+    zero_bank_account.deposit(deposited)
+    zero_bank_account.withdraw(withdrew)
+    assert zero_bank_account.balance == expected
+
+
+def test_insufficient_funds(bank_account):
+    # If we want exception to be raised, not failing the operation is our passed benchmark
+    with pytest.raises(InsufficientFunds): 
+        bank_account.withdraw(200)
