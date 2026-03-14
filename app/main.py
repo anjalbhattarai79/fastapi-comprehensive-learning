3bc8097e6111cbebaa0792b50919322c522f8f21
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2026-03-15 00:36:08 +0545

    Add deploy on Ubuntu Server as well for continuos delivery

--- app/main.py
+++ app/main.py
@@ -26,7 +26,7 @@ app.include_router(vote.router)
    
 @app.get("/") # This will be shown as this is seen at first from TOP-BOTTOM order.
 async def read_root():
-    return {"message": "hello world , Successfully deployed from CI/CD pipeline"}   
+    return {"message": "hello world , Successfully deployed from CI/CD pipeline on both Heroku and Ubuntu server."}   
 
 @app.get("/api2")
 async def read_api2():
