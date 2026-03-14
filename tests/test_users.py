4bad852cb43c0627e26b477ff009b926a66115e7
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2026-03-14 12:33:35 +0545

    Introduce conftest.py file to manage package-specific fixtures

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,21 +1,8 @@
-from fastapi_cloud_cli.cli import app
-
-from .database import client, session   
 from app import models, schemas
 from jose import jwt
 from app.config import settings
-import pytest
 
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "anjal@gmail.com", "password": "password123"}
-    res = client.post("/users", json=user_data)
-    assert res.status_code == 201
-    new_user = res.json()
-    new_user['password'] = user_data['password']
-    return new_user
-    
     
 # def test_root(client):
 #     res = client.get("/")
@@ -42,3 +29,6 @@ def test_login_user(client, test_user):
     assert login_res.token_type == "bearer"    
     
     assert res.status_code == 200
+
+
+
