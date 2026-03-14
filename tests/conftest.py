ae07a6fcb004b27a07b0fc920216a480f35e3339
anjalbhattarai79 <anjalbhattarai79@gmail.com>
2026-03-14 22:57:40 +0545

    Try to find bug

--- tests/conftest.py
+++ tests/conftest.py
@@ -9,7 +9,7 @@ from app.database import get_db
 import pytest
 
 
-SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
